d9c85f75f1cca4d66d84856da16a0720b70b4643
Kevin Lin <kevlin129@hotmail.com>
2021-02-07 12:27:51 +1100

    Test ToC anchor

--- index.js
+++ index.js
@@ -1,31 +1,51 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const promptUser = () => 
 inquirer
   .prompt([
     {
       type: 'input',
-      message: 'What is your user name?',
-      name: 'username',
+      message: 'Your project title: ',
+      name: 'title',
     },
     {
       type: 'input',
-      message: 'What languages do you know?',
-      name: 'languages',
+      message: 'Installation instructions: ',
+      name: 'installation',
     },
     {
       type: 'input',
-      message: 'What is your preferred method of communication?',
-      name: 'communication',
+      message: 'Usage infromation: ',
+      name: 'usage',
     },
-  ])
-  // const generateHTML = (answer) =>
-// Doctype......rest of HTML code...
+    {
+      type: 'input',
+      message: 'Contribution guidelines: ',
+      name: 'contributing',
+    },
+    {
+      type: 'input',
+      message: 'Tests instructions: ',
+      name: 'tests',
+    },
+    {
+      type: 'input',
+      message: 'Your Github username: ',
+      name: 'profile',
+    },
+    {
+      type: 'input',
+      message: 'Your email address: ',
+      name: 'email',
+    },
+  ]);
+
+ 
 
 
-// check screenshot example
-
-
-const html = `<h1>Description</h1>
+const generateHTML = (answers) =>
+`<h1>Description</h1>
 This time planning application allows a user to save events for time blocks (9am to 5pm) in a day. Moment.js was integrated in the script to track current time.
 
 
@@ -39,7 +59,18 @@ https://klin4994.github.io/Daily-Planner/
 </p>
 
 `
+const init = () => {
+    promptUser().then((answers) => {
+        try {
+            const html = generateHTML(answers);
+            fs.writeFileSync('README2.MD', html);
+            console.log('Your README.MD file has been successfully created');
+        }
+        catch (error) {
+            console.log(error);
+        }
+    })
+}
 
-  
-    fs.writeFileSync('log.MD',html)
+init();
     
